332c8ee9ea7193ad638cc1d253e0d6fd6ad9f789
jamies12 <jamie.shaun.simpson12@gmail.com>
2018-12-09 19:26:38 +0000

    Merged changes from dev branch.

--- src/app/project.ts
+++
@@ -1,9 +0,0 @@
-export class Project {
-    id: number;
-    name: string;
-    date: string;
-    url: string;
-    image: string;
-    description: string;
-    languages: string;
-}
--- src/app/projectdetail.ts
+++
@@ -1,9 +0,0 @@
-export class ProjectDetail {
-    id: number;
-    name: string;
-    date: string;
-    url: string;
-    image: string;
-    description: string;
-    languages: string;
-}
